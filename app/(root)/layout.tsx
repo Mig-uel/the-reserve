7f1f1a5685e6da5dc0f8a871788bb4133c673d48
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-02 19:13:09 -0500

    Add Footer component to RootLayout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/footer'
 import Header from '@/components/shared/header'
 import type { Metadata } from 'next'
 
@@ -14,8 +15,10 @@ export default function RootLayout({
   return (
     <div className='flex h-screen flex-col'>
       <Header />
-      
+
       <main className='flex-1 wrapper'>{children}</main>
+
+      <Footer />
     </div>
   )
 }
