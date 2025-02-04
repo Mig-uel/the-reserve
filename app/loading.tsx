73a953c82c2e738d3d49ffd43863b1de4cf7a342
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-04 14:46:06 -0500

    Add Loading component with spinner for loading state indication

---
+++ app/loading.tsx
@@ -0,0 +1,18 @@
+import loader from '@/assets/loader.gif'
+import Image from 'next/image'
+
+export default function Loading() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      <Image src={loader} alt='Loading indicator' height={150} width={150} />
+    </div>
+  )
+}
