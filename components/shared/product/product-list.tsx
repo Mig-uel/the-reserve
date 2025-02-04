a21a460791a0187eff84ad989bf2c000491ba237
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-04 15:40:50 -0500

    Add ProductList component for displaying a list of products with optional limit

---
+++ components/shared/product/product-list.tsx
@@ -0,0 +1,31 @@
+import type { Product } from '@/lib/types'
+
+export default function ProductList({
+  data,
+  title,
+  limit,
+}: {
+  data: Product[]
+  title: string
+  limit?: number
+}) {
+  const limitedData = limit ? data.slice(0, limit) : data
+
+  return (
+    <div className='my-10'>
+      <h2 className='h2-bold mb-4'>{title}</h2>
+
+      {data.length > 0 ? (
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+          {limitedData.map((product) => (
+            <div key={product.description}>{product.name}</div>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No products found</p>
+        </div>
+      )}
+    </div>
+  )
+}
