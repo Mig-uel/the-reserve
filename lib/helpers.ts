390b0cb0520acef7ebefaf58fdf88bf8edc534ca
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-04 20:52:24 -0500

    Add helper function to retrieve latest products from database

---
+++ lib/helpers.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client'
+
+// get latest products
+export async function getLatestProducts() {
+  const prisma = new PrismaClient()
+
+  const data = await prisma.product.findMany({
+    take: 4,
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
+  return data
+}
