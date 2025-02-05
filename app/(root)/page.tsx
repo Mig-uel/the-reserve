02bafc526911fde13f15339c6dd98d249300eeaa
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-04 20:52:49 -0500

    Replace sample data with dynamic retrieval of latest products in the page component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import ProductList from '@/components/shared/product/product-list'
-import sampleData from '@/db/sample-data'
+import { getLatestProducts } from '@/lib/helpers'
 import type { Product } from '@/lib/types'
+import { convertToPlainObject } from '@/lib/utils'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -8,13 +9,13 @@ export const metadata: Metadata = {
 }
 
 export default async function Page() {
+  const latestProducts = convertToPlainObject<Product[]>(
+    await getLatestProducts()
+  )
+
   return (
     <>
-      <ProductList
-        data={sampleData.products as Product[]}
-        title='Newest Arrivals'
-        limit={4}
-      />
+      <ProductList data={latestProducts} title='Newest Arrivals' limit={4} />
     </>
   )
 }
