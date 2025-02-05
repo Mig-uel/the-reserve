35c564c8fe3334f5da6cc8309891af81c7f3410a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-04 20:12:17 -0500

    Integrate ProductList component to display newest arrivals on the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,20 @@
-export default function Page() {
-  return <div>The Reserve</div>
+import ProductList from '@/components/shared/product/product-list'
+import sampleData from '@/db/sample-data'
+import type { Product } from '@/lib/types'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Home',
+}
+
+export default async function Page() {
+  return (
+    <>
+      <ProductList
+        data={sampleData.products as Product[]}
+        title='Newest Arrivals'
+        limit={4}
+      />
+    </>
+  )
 }
