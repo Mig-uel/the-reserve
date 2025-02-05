78efce3ce1c36d4af728200597b35da5cd16c95d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-04 23:10:25 -0500

    Refactor getLatestProducts to use existing Prisma client instance

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,9 +1,7 @@
-import { PrismaClient } from '@prisma/client'
+import { prisma } from '@/db/prisma'
 
 // get latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient()
-
   const data = await prisma.product.findMany({
     take: 4,
     orderBy: {
