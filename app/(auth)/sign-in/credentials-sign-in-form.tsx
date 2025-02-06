0a6efb2a95151d30619ccde1d24665148110a123
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-05 19:31:16 -0500

    Enhance CredentialsSignInForm with action state management and submit button

--- app/(auth)/sign-in/credentials-sign-in-form.tsx
+++ app/(auth)/sign-in/credentials-sign-in-form.tsx
@@ -1,13 +1,21 @@
 'use client'
 
+import SubmitButton from '@/components/shared/form/submit-button'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { signInWithCredentials } from '@/lib/helpers'
 import Link from 'next/link'
+import { useActionState } from 'react'
 
 export default function CredentialsSignInForm() {
+  const [data, action] = useActionState(signInWithCredentials, {
+    success: false,
+    message: '',
+  })
+
   return (
-    <form>
+    <form action={action}>
       <div className='space-y-6'>
         {/* Email input */}
         <div>
@@ -35,9 +43,11 @@ export default function CredentialsSignInForm() {
           ></Input>
         </div>
 
-        <div>
-          <Button className='w-full'>Sign In</Button>
-        </div>
+        <SubmitButton text='Sign In' />
+
+        {data && !data.success && (
+          <div className='text-center text-destructive'>{data.message}</div>
+        )}
 
         <div className='text-sm text-center text-muted-foreground'>
           Don&apos;t have an account?
