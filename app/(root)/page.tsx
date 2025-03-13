ef2235f002240b06be3b3a047c6640054901fc5d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-13 16:59:55 -0400

    Add ProductList component to display newest arrivals
    - Integrate ProductList into Homepage for better product visibility
    - Utilize sample data for initial product listings

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,10 @@
+import ProductList from '@/components/shared/product/product-list'
+import sampleData from '@/db/sample-data'
+
 export default function Homepage() {
-  return <div>Homepage</div>
+  return (
+    <>
+      <ProductList data={sampleData.products} title='Newest Arrivals' />
+    </>
+  )
 }
