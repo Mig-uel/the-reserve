1190742d2ed6666f500cd9cf1cf185e1164f375d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-13 19:53:40 -0400

    Add InsertProductSchema for product validation
    - Introduces a Zod schema for validating product insertion.
    - Ensures price has exactly two decimal places using formatNumberWithDecimal.
    - Validates other product fields like name, slug, category, and images.

---
+++ zod/validators.ts
@@ -0,0 +1,33 @@
+import { formatNumberWithDecimal } from '@/lib/utils'
+import { z } from 'zod'
+
+/**
+ * Schema for Inserting Products
+ */
+const currency = z
+  .string()
+  .refine(
+    (val) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(+val)),
+    'Price must have exactly two decimal places'
+  )
+export const InsertProductSchema = z.object({
+  name: z.string().min(3, 'Name must be at least 3 characters'),
+
+  slug: z.string().min(3, 'Slug must be at least 3 characters'),
+
+  category: z.string().min(3, 'Category must be at least 3 characters'),
+
+  brand: z.string().min(1, 'Brand must be at least 1 characters'),
+
+  description: z.string().min(3, 'Description must be at least 3 characters'),
+
+  stock: z.coerce.number(),
+
+  images: z.array(z.string()).min(1, 'Product must have at least one image'),
+
+  isFeatured: z.boolean(),
+
+  banner: z.string().nullable(),
+
+  price: currency,
+})
