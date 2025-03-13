1a40903d65eb5f0f560613897fb4c37a320c130d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-13 17:15:03 -0400

    Add limit prop to ProductList for displaying items - Introduced a limit prop to control the number of products   displayed in the ProductList component, enhancing user experience.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,11 @@ import sampleData from '@/db/sample-data'
 export default function Homepage() {
   return (
     <>
-      <ProductList data={sampleData.products} title='Newest Arrivals' />
+      <ProductList
+        data={sampleData.products}
+        title='Newest Arrivals'
+        limit={4}
+      />
     </>
   )
 }
