86bbe1799fa0fb3767fe34d9122d293e89754337
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-14 15:12:12 -0400

    Add NextAuth configuration with credentials provider
    - Implement NextAuth with Prisma adapter for user authentication
    - Set up JWT session strategy and custom sign-in page
    - Add credentials provider for email and password authentication

--- auth.ts
+++ auth.ts
@@ -1,5 +1,76 @@
-import NextAuth from 'next-auth'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { compareSync } from 'bcrypt-ts-edge'
+import NextAuth, { NextAuthConfig } from 'next-auth'
+import Credentials from 'next-auth/providers/credentials'
+import { prisma } from './db/prisma'
 
-export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [],
-})
+export const config: NextAuthConfig = {
+  pages: {
+    signIn: '/sign-in',
+    error: '/sign-in',
+  },
+
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60,
+  },
+
+  adapter: PrismaAdapter(prisma),
+
+  providers: [
+    Credentials({
+      credentials: {
+        email: {
+          type: 'email',
+        },
+        password: {
+          type: 'password',
+        },
+      },
+
+      async authorize({ email, password }) {
+        if (email === null || password === null) return null
+
+        // find user in database
+        const user = await prisma.user.findFirst({
+          where: {
+            email: email as string,
+          },
+        })
+
+        //check if user exists and if password matches
+        if (user && user.password) {
+          const isMatch = compareSync(password as string, user.password)
+
+          // if password is correct
+          if (isMatch)
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            }
+        }
+
+        // if user does not exists or password does not match return null
+        return null
+      },
+    }),
+  ],
+
+  callbacks: {
+    async session({ session, token, user, trigger }) {
+      // set the user ID from the token
+      session.user.id = token.sub!
+
+      // if there is an update, set the user's name
+      if (trigger === 'update') {
+        session.user.name = user.name
+      }
+
+      return session
+    },
+  },
+}
+
+export const { auth, handlers, signIn, signOut } = NextAuth(config)
