5e80dd2e31914ad0a1f3ce0f9e25a4fa714f657a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-14 16:05:03 -0400

    Add signInWithCredentials action for user authentication
    - Implements user sign-in logic using credentials
    - Validates input with SignInFormSchema
    - Handles success and error responses

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,36 @@
+'use server'
+
+import { signIn } from '@/auth'
+import { SignInFormSchema } from '@/zod/validators'
+import { isRedirectError } from 'next/dist/client/components/redirect-error'
+
+/**
+ * Sign In User with Credentials
+ */
+export async function signInWithCredentials(
+  prevState: unknown,
+  formData: FormData
+) {
+  try {
+    const user = SignInFormSchema.parse({
+      email: formData.get('email'),
+      password: formData.get('password'),
+    })
+
+    const authUser = await signIn('credentials', user)
+
+    console.log(authUser)
+
+    return {
+      success: true,
+      message: `Sign in successful`,
+    }
+  } catch (error) {
+    if (isRedirectError(error)) throw error
+
+    return {
+      success: false,
+      message: 'Invalid email or password',
+    }
+  }
+}
