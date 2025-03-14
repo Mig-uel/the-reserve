860ee163e70cb7dc755d9b7a174f089dfc314fd1
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-14 18:51:26 -0400

    Add error formatting utility for improved error handling
    - Introduced formatErrors function to handle various error types
    - Supports Zod and Prisma errors with specific messages
    - Enhances user feedback during error scenarios

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,9 @@
+// TODO: improve error handling
+
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 import { NUMBER_FORMATTER } from './constants'
+import { ZodError } from 'zod'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -18,4 +21,36 @@ export function convertToPlainObject<T>(value: T): T {
  */
 export function formatNumberWithDecimal(value: number) {
   return NUMBER_FORMATTER.format(value)
+}
+
+/**
+ * Format Errors
+ */
+export async function formatErrors(
+  error: Error & {
+    code?: string
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    meta?: any
+  }
+) {
+  if (error instanceof ZodError) {
+    // handle zod error
+    const fieldErrors = Object.keys(error.errors).map(
+      (field) => error.errors[field].message
+    )
+
+    return fieldErrors.join('. ')
+  } else if (
+    error.name === 'PrismaClientKnownRequestError' &&
+    error.code === 'P2002'
+  ) {
+    // handle prisma error
+    const field = error.meta?.target ? error.meta.target[0] : 'Field'
+
+    return `${field.charAt(0).toUpperCase() + field.slice(1)} already exists`
+  } else {
+    return typeof error.message === 'string'
+      ? error.message
+      : JSON.stringify(error.message)
+  }
 }
