0dc76ddc93e4ab2bc2a799169a1372649d912a4d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-15 12:28:26 -0400

    Refactor AddToCart component for improved cart management
    - Integrate getUserCart to fetch current cart state
    - Replace button click handlers with form submissions for add/remove actions
    - Simplify item addition and removal logic

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,85 +1,43 @@
-'use client'
-
-// TODO: improve component to ssr
-
 import { Button } from '@/components/ui/button'
-import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions'
-import type { Cart, CartItem } from '@/zod'
+import {
+  addItemToCart,
+  getUserCart,
+  removeItemFromCart,
+} from '@/lib/actions/cart.actions'
+import type { CartItem } from '@/zod'
 import { Minus, Plus } from 'lucide-react'
-import { useRouter } from 'next/navigation'
-import { toast } from 'sonner'
 
-export default function AddToCart({
-  cart,
-  item,
-}: {
-  cart?: Cart
-  item: CartItem
-}) {
-  const router = useRouter()
-
-  const handleAddToCart = async () => {
-    const res = await addItemToCart(item)
-
-    if (res && !res.success) {
-      toast.error(res.message, {
-        invert: true,
-      })
-      return
-    }
-
-    // handle success adding to cart
-    toast.success(res?.message, {
-      action: {
-        label: 'Go to Cart',
-        onClick: () => router.push('/cart'),
-      },
-    })
-
-    return
-  }
+export default async function AddToCart({ item }: { item: CartItem }) {
+  const cart = await getUserCart()
 
   // check if item is in cart
   const existingItem = cart?.items.find((i) => i.productId === item.productId)
 
-  const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.productId)
-
-    if (res && !res.success) {
-      toast.error(res.message, {
-        invert: true,
-      })
-      return
-    }
-
-    // handle success adding to cart
-    toast.success(res?.message, {
-      action: {
-        label: 'Go to Cart',
-        onClick: () => router.push('/cart'),
-      },
-    })
-
-    return
-  }
-
   if (existingItem) {
     return (
-      <div>
-        <Button type='button' variant='outline' onClick={handleRemoveFromCart}>
-          <Minus className='h-4 w-4' />
-        </Button>
+      <>
+        <form action={removeItemFromCart.bind(null, existingItem.productId)}>
+          <Button type='submit' variant='outline'>
+            <Minus className='h-4 w-4' />
+          </Button>
+        </form>
+
         <span className='px-2'>{existingItem.qty}</span>
-        <Button type='button' variant='outline' onClick={handleAddToCart}>
-          <Plus className='h-4 w-4' />
-        </Button>
-      </div>
+
+        <form action={addItemToCart.bind(null, item)}>
+          <Button type='submit' variant='outline'>
+            <Plus className='h-4 w-4' />
+          </Button>
+        </form>
+      </>
     )
   }
 
   return (
-    <Button className='w-full' type='button' onClick={handleAddToCart}>
-      <Plus /> Add to Cart
-    </Button>
+    <form action={addItemToCart.bind(null, item)} className='w-full'>
+      <Button className='w-full' type='submit'>
+        <Plus /> Add to Cart
+      </Button>
+    </form>
   )
 }
