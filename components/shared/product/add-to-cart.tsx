4be3cf07cfedf9460f517cba75c27b590487a8bd
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-15 11:04:22 -0400

    Add removeItemFromCart functionality to product component
    - Implemented logic to remove items from the cart
    - Added UI elements for managing item quantities
    - Enhanced user feedback with success/error notifications

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,13 +1,19 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import { addItemToCart } from '@/lib/actions/cart.actions'
-import type { CartItem } from '@/zod'
-import { Plus } from 'lucide-react'
+import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions'
+import type { Cart, CartItem } from '@/zod'
+import { Minus, Plus } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { toast } from 'sonner'
 
-export default function AddToCart({ item }: { item: CartItem }) {
+export default function AddToCart({
+  cart,
+  item,
+}: {
+  cart?: Cart
+  item: CartItem
+}) {
   const router = useRouter()
 
   const handleAddToCart = async () => {
@@ -29,6 +35,44 @@ export default function AddToCart({ item }: { item: CartItem }) {
     })
 
     return
+  }
+
+  // check if item is in cart
+  const existingItem = cart?.items.find((i) => i.productId === item.productId)
+
+  const handleRemoveFromCart = async () => {
+    const res = await removeItemFromCart(item.productId)
+
+    if (res && !res.success) {
+      toast.error(res.message, {
+        invert: true,
+      })
+      return
+    }
+
+    // handle success adding to cart
+    toast.success(res?.message, {
+      action: {
+        label: 'Go to Cart',
+        onClick: () => router.push('/cart'),
+      },
+    })
+
+    return
+  }
+
+  if (existingItem) {
+    return (
+      <div>
+        <Button type='button' variant='outline' onClick={handleRemoveFromCart}>
+          <Minus className='h-4 w-4' />
+        </Button>
+        <span className='px-2'>{existingItem.qty}</span>
+        <Button type='button' variant='outline' onClick={handleAddToCart}>
+          <Plus className='h-4 w-4' />
+        </Button>
+      </div>
+    )
   }
 
   return (
