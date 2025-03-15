01dde27a6066f1e27ab173479c01644ad86ab92a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-14 21:06:07 -0400

    Add AddToCart component for adding items to the cart
    - Implements a button to add items to the cart
    - Displays success or error notifications using Sonner
    - Navigates to the cart page upon successful addition

---
+++ components/shared/product/add-to-cart.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { addItemToCart } from '@/lib/actions/cart.actions'
+import type { CartItem } from '@/zod'
+import { Plus } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { toast } from 'sonner'
+
+export default function AddToCart({ item }: { item: CartItem }) {
+  const router = useRouter()
+
+  const handleAddToCart = async () => {
+    const res = await addItemToCart(item)
+
+    if (!res.success) {
+      toast.error(res.message, {
+        invert: true,
+      })
+      return
+    }
+
+    // handle success adding to cart
+    toast.success(`${item.name} added to cart`, {
+      action: {
+        label: 'Go to Cart',
+        onClick: () => router.push('/cart'),
+      },
+    })
+
+    return
+  }
+
+  return (
+    <Button className='w-full' type='button' onClick={handleAddToCart}>
+      <Plus /> Add to Cart
+    </Button>
+  )
+}
