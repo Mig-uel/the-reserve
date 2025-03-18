72c64c4b72c4f8efea9148d8fad0d18c739597d4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-18 00:33:44 -0400

    Update CartSubtotal to support final screen display
    - Added optional finalScreen prop to conditionally render
      subtotal information for checkout process.
    - Improved layout for displaying itemized costs including
      items, tax, shipping, and total.

--- components/shared/cart/cart-subtotal.tsx
+++ components/shared/cart/cart-subtotal.tsx
@@ -5,13 +5,53 @@ import type { Cart } from '@/zod'
 import { ArrowRight } from 'lucide-react'
 import Link from 'next/link'
 
-export default function CartSubtotal({ cart }: { cart: Cart }) {
+export default function CartSubtotal({
+  cart,
+  finalScreen,
+}: {
+  cart: Cart
+  finalScreen?: boolean
+}) {
+  if (!finalScreen)
+    return (
+      <Card>
+        <CardContent className='p-4 gap-4'>
+          <div className='pb-3 text-xl'>
+            Subtotal ({cart.items.reduce((a, c) => a + c.qty, 0)}):{' '}
+            <span className='font-bold'>{formatCurrency(cart.itemsPrice)}</span>
+          </div>
+
+          <Button asChild className='w-full'>
+            <Link href='/shipping'>
+              <ArrowRight className='w-4 h-4' />
+              Proceed to Checkout
+            </Link>
+          </Button>
+        </CardContent>
+      </Card>
+    )
+
   return (
     <Card>
-      <CardContent className='p-4 gap-4'>
-        <div className='pb-3 text-xl'>
-          Subtotal ({cart.items.reduce((a, c) => a + c.qty, 0)}):{' '}
-          <span className='font-bold'>{formatCurrency(cart.itemsPrice)}</span>
+      <CardContent className='p-4 gap-4 space-y-4'>
+        <div className='flex justify-between'>
+          <div>Items</div>
+          <div>{formatCurrency(cart.itemsPrice)}</div>
+        </div>
+
+        <div className='flex justify-between'>
+          <div>Tax</div>
+          <div>{formatCurrency(cart.taxPrice)}</div>
+        </div>
+
+        <div className='flex justify-between'>
+          <div>Shipping</div>
+          <div>{formatCurrency(cart.shippingPrice)}</div>
+        </div>
+
+        <div className='flex justify-between'>
+          <div>Total</div>
+          <div>{formatCurrency(cart.totalPrice)}</div>
         </div>
 
         <Button asChild className='w-full'>
