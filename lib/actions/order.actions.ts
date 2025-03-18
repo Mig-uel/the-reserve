b20479a2bc19bf391b08fe958d958a186b38a7a7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-18 02:01:41 -0400

    Add createOrder function for order placement flow
    - Implements order creation with validation for user session,
      address, and payment method.
    - Handles cart item processing and clears the cart post-order.
    - Redirects users based on validation checks and order status.

---
+++ lib/actions/order.actions.ts
@@ -0,0 +1,85 @@
+// TODO: send toasts to the user when an error occurs
+'use server'
+
+import { auth } from '@/auth'
+import { prisma } from '@/db/prisma'
+import { InsertOrderSchema } from '@/zod/validators'
+import { isRedirectError } from 'next/dist/client/components/redirect-error'
+import { redirect } from 'next/navigation'
+import { getUserCart } from './cart.actions'
+import { getUserById } from './user.action'
+import { revalidatePath } from 'next/cache'
+
+/**
+ * Create Order Item and Order
+ */
+export async function createOrder(formData: FormData) {
+  try {
+    const session = await auth()
+    if (!session || !session.user || !session.user.id)
+      throw new Error('Must be signed in to place an order')
+
+    const cart = await getUserCart()
+    if (!cart || !cart.items.length) return redirect('/')
+
+    const userId = session.user.id
+
+    const user = await getUserById(userId)
+    if (!user) return redirect('/')
+    if (!user.address) return redirect('/shipping')
+    if (!user.paymentMethod) return redirect('/payment')
+
+    // create order object
+    const order = InsertOrderSchema.parse({
+      userId: user.id,
+      shippingAddress: user.address,
+      paymentMethod: user.paymentMethod,
+      itemsPrice: cart.itemsPrice,
+      shippingPrice: cart.shippingPrice,
+      taxPrice: cart.taxPrice,
+      totalPrice: cart.totalPrice,
+    })
+
+    // create a transaction to create order and order items in the database
+    const insertedOrderId = await prisma.$transaction(async function (tx) {
+      // create order
+      const insertedOrder = await tx.order.create({ data: order })
+
+      // create order items
+      cart.items.forEach(async (item) => {
+        await tx.orderItem.create({
+          data: {
+            ...item,
+            orderId: insertedOrder.id,
+          },
+        })
+      })
+
+      // clear user cart
+      await tx.cart.update({
+        where: { id: cart.id },
+        data: {
+          items: [],
+          totalPrice: 0,
+          shippingPrice: 0,
+          taxPrice: 0,
+          itemsPrice: 0,
+        },
+      })
+
+      return insertedOrder.id
+    })
+
+    if (!insertedOrderId)
+      throw new Error('Order was not placed, please try again')
+
+    // TODO: disable place order button if order was placed successfully
+    revalidatePath('/place-order')
+
+    return redirect(`/order/${insertedOrderId}`)
+  } catch (error) {
+    if (isRedirectError(error)) throw error
+
+    console.log(error)
+  }
+}
