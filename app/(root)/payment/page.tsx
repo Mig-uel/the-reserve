5cd91ef883e92d81aefddf458c5b7153124fe765
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-17 21:41:26 -0400

    Add PaymentPage component for selecting payment method
    - Introduces a new PaymentPage component to facilitate user
      selection of payment methods.
    - Integrates CheckoutSteps and PaymentForm components for
      improved user experience.

---
+++ app/(root)/payment/page.tsx
@@ -0,0 +1,26 @@
+import CheckoutSteps from '@/components/shared/checkout-steps'
+import { MiniSpinner } from '@/components/shared/spinner'
+import type { Metadata } from 'next'
+import { Suspense } from 'react'
+import PaymentForm from './payment-form'
+
+export const metadata: Metadata = {
+  title: 'Payment Method',
+}
+
+export default function PaymentPage() {
+  return (
+    <>
+      <h2 className='h2-bold mt-4'>Payment Method</h2>
+      <p className='text-sm text-muted-foreground'>
+        Please select your payment method
+      </p>
+
+      <CheckoutSteps current={2} />
+
+      <Suspense fallback={<MiniSpinner />}>
+        <PaymentForm />
+      </Suspense>
+    </>
+  )
+}
