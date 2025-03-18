75fc98349e368b11bc7edfdf99b4e98d7a79fb5e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-17 23:50:53 -0400

    Wrap CartTable in a grid for improved layout
    - Enhances the visual structure of the cart page
    - Prepares for potential future layout adjustments

--- app/(root)/cart/page.tsx
+++ app/(root)/cart/page.tsx
@@ -10,7 +10,9 @@ export default function CartPage() {
     <div>
       <h1 className='py-4 h2-bold'>Your Cart</h1>
 
-      <CartTable />
+      <div className='grid md:grid-cols-4 md:gap-5'>
+        <CartTable />
+      </div>
     </div>
   )
 }
