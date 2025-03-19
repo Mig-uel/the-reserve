7fc7e4693c62c7bc6c2e931f64b166d3dce5a72f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-19 17:21:58 -0400

    Add update profile form component for user profiles
    - Implements a form for users to update their profile information
    - Redirects to login if the user is not authenticated
    - Pre-fills email field and allows name input

---
+++ app/user/profile/update-profile-form.tsx
@@ -0,0 +1,41 @@
+import { auth } from '@/auth'
+import SubmitButton from '@/components/submit-button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { updateUserProfile } from '@/lib/actions/user.action'
+import { redirect } from 'next/navigation'
+
+export default async function UpdateProfileForm() {
+  const session = await auth()
+
+  if (!session || !session.user || !session.user.email)
+    return redirect('/login')
+
+  const { user } = session
+
+  return (
+    <form className='space-y-4' action={updateUserProfile}>
+      <div className='flex flex-col gap-2'>
+        <Label htmlFor='email'>Email</Label>
+        <Input
+          name='email'
+          type='email'
+          defaultValue={user.email as string}
+          disabled
+        />
+      </div>
+
+      <div className='flex flex-col gap-2'>
+        <Label htmlFor='name'>Name</Label>
+        <Input
+          name='name'
+          type='text'
+          defaultValue={user.name as string}
+          placeholder='Enter your name'
+        />
+      </div>
+
+      <SubmitButton>Save Profile</SubmitButton>
+    </form>
+  )
+}
