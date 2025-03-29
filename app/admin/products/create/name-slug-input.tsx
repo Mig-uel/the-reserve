a04cc3359e8a32edaee842b03bc9e786614d237c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-28 22:23:32 -0400

    Add NameSlugInput component for product creation
    - Introduces a new input component for entering product name
    - Automatically generates a slug based on the product name
    - Enhances user experience by providing real-time slug updates

---
+++ app/admin/products/create/name-slug-input.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { useState } from 'react'
+
+export default function NameSlugInput() {
+  const [name, setName] = useState('')
+  const [slug, setSlug] = useState('')
+
+  return (
+    <>
+      <div className='flex flex-col md:flex-row gap-5'>
+        {/* Product Name */}
+        <div className='w-full'>
+          <Label htmlFor='name'>Name</Label>
+          <Input
+            id='name'
+            name='name'
+            type='text'
+            placeholder='Product Name'
+            required
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value)
+              setSlug(e.target.value.toLowerCase().replace(/ /g, '-'))
+            }}
+          />
+        </div>
+      </div>
+
+      <div className='flex flex-col md:flex-row gap-5'>
+        {/* Product Slug */}
+        <div className='w-full'>
+          <Label htmlFor='slug'>Slug</Label>
+          <Input
+            id='slug'
+            name='slug'
+            type='text'
+            placeholder='Product Slug'
+            required
+            disabled
+            value={slug}
+          />
+        </div>
+      </div>
+    </>
+  )
+}
