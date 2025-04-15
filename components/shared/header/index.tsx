d359c00532045fdf07c1aaebc195490f907b2e47
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-15 12:14:18 -0400

    feat: add Search component to Header for product search
    - Integrates a search form with category selection and input field
    - Enhances user experience by allowing product searches directly from the header

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Menu from './menu'
 import CategoryDrawer from './category-drawer'
+import Search from './search'
 
 export default function Header() {
   return (
@@ -24,6 +25,9 @@ export default function Header() {
           </Link>
         </div>
 
+        <div className='hidden md:block'>
+          <Search />
+        </div>
         <Menu />
       </div>
     </header>
