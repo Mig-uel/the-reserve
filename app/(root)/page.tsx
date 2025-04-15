301791d7511d89373fa90a2a6f841c23f1c5aafe
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-15 12:04:09 -0400

    feat: add featured products to Homepage with ProductCarousel
    - Integrate ProductCarousel to display featured products.
    - Fetch featured products alongside latest products for better visibility.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,21 @@
+import ProductCarousel from '@/components/shared/product/product-carousel'
 import ProductList from '@/components/shared/product/product-list'
-import { getLatestProducts } from '@/lib/actions/product.actions'
+import {
+  getFeaturedProducts,
+  getLatestProducts,
+} from '@/lib/actions/product.actions'
 
 export default async function Homepage() {
   const latestProducts = await getLatestProducts()
+  const featuredProducts = await getFeaturedProducts()
 
   return (
     <>
+      {featuredProducts.length ? (
+        // @ts-expect-error fix type later */}
+        <ProductCarousel data={featuredProducts} />
+      ) : null}
+
       {/* @ts-expect-error fix type later */}
       <ProductList data={latestProducts} title='Newest Arrivals' limit={4} />
     </>
