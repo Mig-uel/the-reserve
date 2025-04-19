a590929113a90e96e9dac296c758fa2d896142dc
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-19 10:16:03 -0400

    feat: add Stripe success page for order processing
    - Implemented a new page to handle successful Stripe payments
    - Fetches order details and validates payment intent
    - Provides user feedback upon successful purchase

---
+++ app/(root)/order/[orderId]/stripe-payment-success/page.tsx
@@ -0,0 +1,54 @@
+import { Button } from '@/components/ui/button'
+import { getOrderById } from '@/lib/actions/order.actions'
+import Link from 'next/link'
+import { notFound, redirect } from 'next/navigation'
+import Stripe from 'stripe'
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '')
+
+type Props = {
+  params: Promise<{ orderId: string }>
+  searchParams: Promise<{ payment_intent: string }>
+}
+
+export default async function StripeSuccessPage({
+  params,
+  searchParams,
+}: Props) {
+  const { orderId } = await params
+  const { payment_intent: payment_intent_id } = await searchParams
+
+  // fetch order
+  const order = await getOrderById(orderId)
+  if (!order) return notFound()
+
+  // retrieve payment intent
+  const paymentIntent = await stripe.paymentIntents.retrieve(payment_intent_id)
+
+  // check if payment intent is valid
+  if (
+    paymentIntent.metadata.orderId === null ||
+    paymentIntent.metadata.orderId !== order.id
+  ) {
+    return notFound()
+  }
+
+  // check if payment was successful
+  const isPaymentSuccessful = paymentIntent.status === 'succeeded'
+
+  if (!isPaymentSuccessful) {
+    return redirect(`/order/${orderId}`)
+  }
+
+  return (
+    <div className='max-w-4xl w-full mx-auto space-y-8'>
+      <div className='flex flex-col gap-6 items-center'>
+        <h1 className='h1-bold'>Thanks for your purchase</h1>
+        <div>We are processing your order.</div>
+        <Button asChild>
+          <Link href={`/order/${orderId}`}>View Order</Link>
+        </Button>
+      </div>
+    </div>
+  )
+}
