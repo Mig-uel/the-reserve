b46c31f2fdbe221086c5288b5e93d0f625ef8f2a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-19 12:48:41 -0400

    feat: add IconBoxes component to showcase key features
    - Introduced IconBoxes component to highlight benefits
    - Includes icons and descriptions for Free Shipping, Money Back Guarantee, Flexible Payment, and 24/7 Support

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import IconBoxes from '@/components/icon-boxes'
 import ProductCarousel from '@/components/shared/product/product-carousel'
 import ProductList from '@/components/shared/product/product-list'
 import ViewAllProductsButton from '@/components/view-all-products-button'
@@ -21,6 +22,8 @@ export default async function Homepage() {
       <ProductList data={latestProducts} title='Newest Arrivals' limit={4} />
 
       <ViewAllProductsButton />
+
+      <IconBoxes />
     </>
   )
 }
