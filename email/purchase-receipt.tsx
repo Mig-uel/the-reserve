a5f18afbde23a42c1734a287a25a8b693d639fa4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-19 11:48:55 -0400

    feat: enhance PurchaseReceiptEmail with order details
    - Display order ID, purchase date, and price paid
    - List order items with images and quantities
    - Format prices using utility function

--- email/purchase-receipt.tsx
+++ email/purchase-receipt.tsx
@@ -1,5 +1,116 @@
+import { formatCurrency } from '@/lib/utils'
 import type { Order } from '@/zod'
+import {
+  Body,
+  Column,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Img,
+  Preview,
+  Row,
+  Section,
+  Tailwind,
+  Text,
+} from '\@react-email/components'
+
+const dateFormatter = new Intl.DateTimeFormat('en-US', {
+  dateStyle: 'medium',
+})
 
 export default function PurchaseReceiptEmail({ order }: { order: Order }) {
-  return <div>Purchase Receipt for Order #{order.id}</div>
+  return (
+    <Html>
+      <Preview>View order receipt</Preview>
+      <Tailwind>
+        <Head />
+        <Body className='font-sans bg-white'>
+          <Container className='max-w-xl'>
+            <Heading>Purchase Receipt</Heading>
+
+            <Section>
+              <Row>
+                <Column>
+                  <Text className='mb-0 mr-4 text-gray-500 whitespace-nowrap text-nowrap'>
+                    Order ID
+                  </Text>
+                  <Text className='mt-0 mr-4'>{order.id.toString()}</Text>
+                </Column>
+
+                <Column>
+                  <Text className='mb-0 mr-4 text-gray-500 whitespace-nowrap text-nowrap'>
+                    Purchase Date
+                  </Text>
+                  <Text className='mt-0 mr-4'>
+                    {/* @ts-expect-error fix arg type later */}
+                    {dateFormatter.format(order.createdAt)}
+                  </Text>
+                </Column>
+
+                <Column>
+                  <Text className='mb-0 mr-4 text-gray-500 whitespace-nowrap text-nowrap'>
+                    Price Paid
+                  </Text>
+                  <Text className='mt-0 mr-4'>
+                    {formatCurrency(order.totalPrice)}
+                  </Text>
+                </Column>
+              </Row>
+            </Section>
+
+            <Section className='border border-solid border-gray-500 rounded-lg p-4 md:p-6 my-4'>
+              {order.orderItems.map((item) => (
+                <Row key={item.productId} className='mt-8'>
+                  <Column className='w-20'>
+                    <Img
+                      width={80}
+                      alt={item.name}
+                      src={
+                        item.image.startsWith('/')
+                          ? `${process.env.NEXT_PUBLIC_SERVER_URL}${item.image}`
+                          : item.image
+                      }
+                    />
+                  </Column>
+                  <Column className='align-top'>
+                    {item.name} x {item.qty}
+                  </Column>
+                  <Column className='align-top' align='right'>
+                    {formatCurrency(item.price)}
+                  </Column>
+                </Row>
+              ))}
+
+              {[
+                {
+                  name: 'Items',
+                  price: order.itemsPrice,
+                },
+                {
+                  name: 'Tax',
+                  price: order.taxPrice,
+                },
+                {
+                  name: 'Shipping',
+                  price: order.shippingPrice,
+                },
+                {
+                  name: 'Total',
+                  price: order.totalPrice,
+                },
+              ].map((i) => (
+                <Row key={i.name} className='py-1'>
+                  <Column align='right'>{i.name}: </Column>
+                  <Column align='right' width={70} className='align-top'>
+                    <Text className='m-0'>{formatCurrency(i.price)}</Text>
+                  </Column>
+                </Row>
+              ))}
+            </Section>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  )
 }
