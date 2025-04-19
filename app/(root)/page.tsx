46c930a6ce1152e21c32f7b2d83a8c0d71dde781
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-19 13:07:18 -0400

    feat: add DealCountdown component for promotional offers
    - Introduces a countdown timer for limited-time deals
    - Enhances user engagement by displaying time remaining
    - Provides a visual cue for ongoing promotions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import DealCountdown from '@/components/deal-countdown'
 import IconBoxes from '@/components/icon-boxes'
 import ProductCarousel from '@/components/shared/product/product-carousel'
 import ProductList from '@/components/shared/product/product-list'
@@ -22,6 +23,8 @@ export default async function Homepage() {
       <ProductList data={latestProducts} title='Newest Arrivals' limit={4} />
 
       <ViewAllProductsButton />
+
+      <DealCountdown />
 
       <IconBoxes />
     </>
