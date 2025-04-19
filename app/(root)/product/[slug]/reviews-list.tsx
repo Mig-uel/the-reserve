d8af36a3d559f9b714a65e1ab3c3ebdf69a302cd
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-04-19 07:50:07 -0400

    feat: display reviews with Rating component in ReviewsList
    - Added Rating component to show review ratings.
    - Enhanced review display with user information and timestamps.
    - Improved layout for better readability of reviews.

--- app/(root)/product/[slug]/reviews-list.tsx
+++ app/(root)/product/[slug]/reviews-list.tsx
@@ -3,6 +3,16 @@ import { Button } from '@/components/ui/button'
 import { getAllReviews, getUserReview } from '@/lib/actions/review.actions'
 import Link from 'next/link'
 import ReviewForm from './review-form'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Calendar, UserIcon } from 'lucide-react'
+import { formatDateTime } from '@/lib/utils'
+import Rating from '@/components/rating'
 
 export default async function ReviewsList({
   productId,
@@ -38,7 +48,36 @@ export default async function ReviewsList({
         </div>
       )}
 
-      <div className='flex flex-col gap-3'>{/* TODO: reviews here */}</div>
+      <div className='flex flex-col gap-3'>
+        <p>Total Reviews: {reviews.length}</p>
+
+        {reviews.map((review) => (
+          <Card key={review.id}>
+            <CardHeader>
+              <div className='flex-between'>
+                <CardTitle>{review.title}</CardTitle>
+              </div>
+              <CardDescription>{review.description}</CardDescription>
+            </CardHeader>
+
+            <CardContent>
+              <div className='flex space-x-4 text-sm text-muted-foreground'>
+                <Rating value={review.rating} />
+
+                <div className='flex items-center'>
+                  <UserIcon className='mr-1 h-3 w-3' />
+                  {review.user ? review.user.name : 'Deleted User'}
+                </div>
+
+                <div className='flex items-center'>
+                  <Calendar className='mr-1 h-3 w-3' />
+                  {formatDateTime(review.createdAt).dateTime}
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
     </div>
   )
 }
